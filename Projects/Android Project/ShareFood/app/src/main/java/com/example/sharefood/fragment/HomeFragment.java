fe711e819c66fa7cd8cb1793944296013da4e5cc
Fernando Augusto <f3rnand0_lp@hotmail.com>
2019-12-29 22:35:57 -0300

    Footer corrigido

--- Projects/Android Project/ShareFood/app/src/main/java/com/example/sharefood/fragment/HomeFragment.java
+++ Projects/Android Project/ShareFood/app/src/main/java/com/example/sharefood/fragment/HomeFragment.java
@@ -2,8 +2,10 @@ package com.example.sharefood.fragment;
 
 import android.os.Bundle;
 import android.view.LayoutInflater;
+import android.view.MotionEvent;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.EditText;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -16,6 +18,31 @@ public class HomeFragment extends Fragment {
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_home, container, false);
+        View view =  inflater.inflate(R.layout.fragment_home, container, false);
+
+        final EditText searchEditText = view.findViewById(R.id.search_edit_text);
+        searchEditText.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View view, MotionEvent event) {
+                final int DRAWABLE_LEFT = 0;
+                final int DRAWABLE_TOP = 1;
+                final int DRAWABLE_RIGHT = 2;
+                final int DRAWABLE_BOTTOM = 3;
+
+                if(event.getAction() == MotionEvent.ACTION_UP){
+                    if(event.getRawX() >= (searchEditText.getRight() - searchEditText.getCompoundDrawables()[DRAWABLE_RIGHT].getBounds().width())) {
+                        // your action here
+                        System.out.println("Pesquisar por voz");
+
+                        return true;
+                    }
+                }
+
+                return false;
+            }
+        });
+
+
+        return view;
     }
 }
