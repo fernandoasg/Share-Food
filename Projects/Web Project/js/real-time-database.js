43d3e433896ab5d569d5d9762758dc1c7a464ecc
Fernando Augusto <f3rnand0_lp@hotmail.com>
2020-04-07 22:13:56 -0300

    Coisas do Pre Cadastro

--- Projects/Web Project/js/real-time-database.js
+++ Projects/Web Project/js/real-time-database.js
@@ -1,11 +1,28 @@
-db.collection("users").add({
-    first: "Ada",
-    last: "Lovelace",
-    born: 1815
-})
-.then(function(docRef) {
-    console.log("Document written with ID: ", docRef.id);
-})
-.catch(function(error) {
-    console.error("Error adding document: ", error);
-});
+//var nome = document.getElementById('');
+
+/*preCadastrarBotao.addEventListener('click', function (){
+
+});*/
+
+
+function salvarPreCadastro(){
+    console.log("asdasdasdds")
+    var nome = document.getElementById('nomePre').value
+    var eDoador = document.getElementById('radioDoador').checked
+    var email = document.getElementById('emailPre').value
+
+    var data = new Date()
+    
+    db.collection("PreCadastro").add({
+        Doador: eDoador,
+        Email: email,
+        Nome: nome
+    })
+    .then(function(docRef) {
+        console.log("Document written with ID: ", docRef.id);
+        $('#preCadastro').modal('hide');
+    })
+    .catch(function(error) {
+        console.error("Error adding document: ", error);
+    });
+}
